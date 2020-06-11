226d088cb68a8e9bbc4e109212448f7be226ed52
Carlos <ingcarlostello@hotmail.com>
2020-06-11 18:17:42 -0500

    last retouchs to Navbar component finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,10 +20,13 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="true">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="true">Home<span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="true">Link</a>
+                            <a className="nav-link" href="true">Add Movie</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="true">Top 5<span className="sr-only">(current)</span></a>
                         </li>
                     </ul>
                 </div>
---
+++ src/components/TopFive.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const TopFive = () => {
+    return (
+        <div>
+            <h1>From Top Five Page</h1>
+        </div>
+    );
+};
+
+export default TopFive;
