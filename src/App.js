d6c0f8a8697553138e43432cc2997e4854d519f4
Carlos <ingcarlostello@hotmail.com>
2020-06-11 18:25:14 -0500

    importing components to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AddMovie from './components/AddMovie';
+import TopFive from './components/TopFive';
 import './scss/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <Home />
+      <AddMovie />
+      <TopFive />
     </div>
   );
 }
