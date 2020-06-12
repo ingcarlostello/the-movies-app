027613072c8f5b6d6f2c6a6674c9d99a08e42a33
Carlos <ingcarlostello@hotmail.com>
2020-06-12 07:40:08 -0500

    basic columns structure for components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../scss/Home.scss'
 
 const Home = () => {
     return (
-        <div>
-            <h1>From Home Page</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div class="col-3 sidebar">
+                    col 4
+                </div>
+                <div class="col-8">
+                   col 8
+                </div>
+                {/* <nav className="col-md-3 col-lg-2 d-md-block sidebar">
+                </nav>           */}
+            </div>
         </div>
+        
     );
 };
 
