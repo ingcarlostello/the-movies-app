9edfa09e1c9e6e7efd6086a7817e9f84ed14601d
Carlos <ingcarlostello@hotmail.com>
2020-06-12 23:15:32 -0500

    design completely finished

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -5,32 +5,31 @@ import '../scss/MovieDetail.scss';
 
 const MovieDetail = () => {
     return (
-        <div className="movieDetail-wrapper">
-
+        <div className="container-fluid movieDetail-container">
             <div className="button-wrapper">
                 <Link to="/add-movie">
                     <button type="button" class="btn btn-primary"><span><i class="fas fa-plus"></i></span>Add New Movie</button>
                 </Link>
             </div>
 
-            <div class="row row-cols-1 row-cols-md-1 wrapper-card">
-                <div class="col mb-4 movieDetail-col">
-                    <div class="card movieDetail-card w-50">
-                        <div class="card-header">
-                            <h5>Avengers End Game</h5>
-                            <div className="card-header-date">
-                                <p>19/05/2019</p>
-                            </div>
-                        </div>
-                        <img src={image} class="card-img-top" alt="..." />
-                        <div class="card-body">                           
-                            <p class="card-text"><strong>Movie Description:</strong> This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
+            <div className="movieDetail__card">
+                <div className="movieDetail__card-header">
+                    <h2>Avengers End Game</h2>
+                    <div className="card-header-date">
+                        <p>19/05/2019</p>
                     </div>
+                </div>
+                <div className="movieDetail__card-wrapper-photo">
+                    <div className="movieDetail__card-img">
+                        <img src={image} className="movieDetail__card-photo" alt="..." />
+                    </div>
+                </div>
+                <div className="movieDetail__card-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare est ultricies, pretium justo eu, malesuada quam. In ornare libero et nulla vestibulum maximus.</p>
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 export default MovieDetail;
